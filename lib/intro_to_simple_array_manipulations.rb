2170446ebad23fab98a5292222e4242c2dd3cb3c
Bri Turner <blturner3527@gmail.com>
2020-01-09 03:20:17 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,8 +26,8 @@ def using_concat(array1, array2)
   array1.concat(array2)
 end
 
-def using_insert(array)
-  array[4].insert
+def using_insert(array, element)
+  array[4].insert(element)
 end
 
 def using_uniq(array)
